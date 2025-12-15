5081065e2c21813a7977aa46a1861738458ba0c7
huha3 <anisahsna3@gmail.com>
2025-12-15 16:22:16 +0700

    fix: update layout geocade

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import { AppSidebar } from "@/components/app-sidebar"; 
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>{children}</SidebarInset>
+    </SidebarProvider>
+  );
+}
